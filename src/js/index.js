dbc42b5a5f792c73a3f919139810205f5e3d2fbc
Qexat <43090614+qexat@users.noreply.github.com>
2026-07-17 19:57:42 +0200

    use `getComputedStyle()` instead of `.style`

--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,18 @@ if (previousPage !== null) {
     const previousBanner = document.getElementById(`banner-${previousPage}`);
 
     if (previousBanner !== null) {
-        const finalTransitionDuration = previousBanner.style.transitionDuration;
-        const finalHeight = previousBanner.style.height;
+        const previousBannerStyle = getComputedStyle(previousBanner);
+        const finalTransitionDuration = previousBannerStyle.transitionDuration;
+        const finalHeight = previousBannerStyle.height;
+
         previousBanner.style.transitionDuration = "0ms";
         previousBanner.style.height = "100cqh";
 
         for (let banner of banners) {
             if (banner !== previousBanner) {
-                const finalTransitionDuration = banner.style.transitionDuration;
-                const finalHeight = banner.style.height;
+                const bannerStyle = getComputedStyle(banner);
+                const finalTransitionDuration = bannerStyle.transitionDuration;
+                const finalHeight = bannerStyle.height;
                 banner.style.transitionDuration = "0ms";
                 banner.style.height = "0cqh";
 
